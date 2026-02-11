16c2fcd9e75c9404f6edcd26388f83f08fa6aa0b
Anubhav Amol Jha <228481946+anubhav-amol@users.noreply.github.com>
2026-02-11 11:13:53 +0530

    feat(client): implement workflow list with mock api

--- client/src/pages/WorkflowList.jsx
+++ client/src/pages/WorkflowList.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { getWorkflows } from "../api/workflow.api";
+
 function WorkflowList() {
-  return <h2>WorkflowList Page</h2>;
+  const [workflows, setWorkflows] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getWorkflows();
+      setWorkflows(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Workflows</h2>
+
+      {workflows.length === 0 ? (
+        <p>No workflows found.</p>
+      ) : (
+        <ul>
+          {workflows.map((workflow) => (
+            <li key={workflow.id}>
+              <h3>{workflow.name}</h3>
+              <p>{workflow.description}</p>
+              <small>Created: {workflow.createdAt}</small>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default WorkflowList;
+
